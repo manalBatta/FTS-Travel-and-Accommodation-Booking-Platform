b7bb0cdc8299af63a0e2ffefc325e7ba095dea61
manalBatta <95074111+manalBatta@users.noreply.github.com>
2024-11-10 07:28:58 +0200

    basic search bar

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,29 +1,38 @@
 import "./HomePage.css";
+import Navbar from "./Navbar/Navbar";
+import { IoIosSearch } from "react-icons/io";
 const HomePage = () => {
   return (
     <div className="home-page">
       <header className="header-container">
-        <nav>
-          <img src="./logo.png" alt="2risem website Logo" className="logo" />
-          <ul>
-            <li>
-              <a href="#">Search</a>
-            </li>
-            <li>
-              <a href="#Featured">Featured Deals</a>
-            </li>{" "}
-            <li>
-              <a href="#Trending"> Trending Destination</a>
-            </li>
-            <li>
-              <a href="#Recently">Recently Visited</a>
-            </li>
-          </ul>
-          <div className="user-icon">
-            <h2>Eva jonson</h2>{" "}
-            <img src="./userIcon.png" alt="Female user icon" />
-          </div>
-        </nav>
+        <Navbar></Navbar>
+        <div className="search-container">
+          <h1>Discover the most engaging places</h1>
+          <section className="search-bar">
+            <IoIosSearch className="search-icon" />
+            <input
+              type="text"
+              placeholder="Search for hotels, cities..."
+              className="search-input"
+            />
+            <div className="search-details">
+              <h2>When to Pick-up </h2>
+              <input type="date" className="search-input" />
+            </div>
+            <div className="search-details">
+              <h2>When to Drop-off </h2>
+              <input type="date" className="search-input" />
+            </div>
+            <div className="search-details w-100">
+              <h2>Gusts </h2>
+              <input type="number" className="search-input" value={2} />
+            </div>
+            <div className="search-details w-100">
+              <h2>Rooms </h2>
+              <input type="number" className="search-input" value={1} />
+            </div>
+          </section>
+        </div>
       </header>
       <main>main content</main>
       <footer>footer copy write manal</footer>
---
+++ src/components/HomePage/Navbar/Navbar.tsx
@@ -0,0 +1,28 @@
+import "./Navbar.css";
+const Navbar = () => {
+  return (
+    <nav>
+      <img src="./logo.png" alt="2risem website Logo" className="logo" />
+      <ul>
+        <li>
+          <a href="#">Search</a>
+        </li>
+        <li>
+          <a href="#Featured">Featured Deals</a>
+        </li>{" "}
+        <li>
+          <a href="#Trending"> Trending Destination</a>
+        </li>
+        <li>
+          <a href="#Recently">Recently Visited</a>
+        </li>
+      </ul>
+      <div className="user-icon">
+        <h2>Eva jonson</h2>
+        <img src="./userIcon.png" alt="Female user icon" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
