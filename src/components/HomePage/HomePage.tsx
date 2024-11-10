1fd9328c2d0e7d78ea7f4cc66f19b9acc2e0a5b1
manalBatta <95074111+manalBatta@users.noreply.github.com>
2024-11-10 04:42:52 +0200

    basic Home page design

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,4 +1,33 @@
+import "./HomePage.css";
 const HomePage = () => {
-  return <div>Hello world!</div>;
+  return (
+    <div className="home-page">
+      <header className="header-container">
+        <nav>
+          <img src="./logo.png" alt="2risem website Logo" className="logo" />
+          <ul>
+            <li>
+              <a href="#">Search</a>
+            </li>
+            <li>
+              <a href="#Featured">Featured Deals</a>
+            </li>{" "}
+            <li>
+              <a href="#Trending"> Trending Destination</a>
+            </li>
+            <li>
+              <a href="#Recently">Recently Visited</a>
+            </li>
+          </ul>
+          <div className="user-icon">
+            <h2>Eva jonson</h2>{" "}
+            <img src="./userIcon.png" alt="Female user icon" />
+          </div>
+        </nav>
+      </header>
+      <main>main content</main>
+      <footer>footer copy write manal</footer>
+    </div>
+  );
 };
 export default HomePage;
